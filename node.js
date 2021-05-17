a39a6ee70864fae0455144eff9b6276305506245
Jayapreetha N <69394331+jayapreetha21@users.noreply.github.com>
2021-05-17 17:23:51 +0530

    Add files via upload

---
+++ node.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const bodyParser= require("body-parser")
+
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res){
+
+    var weight= Number(req.body.weight);
+    var height = Number(req.body.height);
+
+   
+    var result =  weight / (height/100)**2;
+  
+
+    res.send("Result: " + result);
+})
+
+app.listen(3000, function(){
+    console.log("Server is running on port 3000.");
+})
